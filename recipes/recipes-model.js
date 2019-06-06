96c005b595ea896e810a70b5ffdf48d747dee8ec
Ming Liu <mliu@macpro.home>
2019-06-06 18:23:37 -0400

    add more helper function and endpoints

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -10,7 +10,9 @@ const db = knex(knexConfig.development);
 
 module.exports = {
     getRecipes,
-    addRecipe
+    addRecipe,
+    addIngredient,
+    getRecipe
 }
 
 function getRecipes() {
@@ -20,7 +22,28 @@ function getRecipes() {
     .leftJoin('dishes', 'recipes.dish_id', 'dishes.id')
 }
 
+//add recipe including instruction
 function addRecipe(recipe) {
     return db('recipes')
     .insert(recipe, 'id')
+}
+
+//add ingredient to recipe one at a time
+//require recipe id, ingredient id, quantity and unit id
+function addIngredient(ingredient) {
+    return db('recipe_ingredients')
+    .insert(ingredient, 'id')
+    .then( res => {
+        return getRecipe(ingredient.recipe_id)
+    })
+}
+
+function getRecipe(id) {
+    return db
+    .select('recipes.name as recipe', 'ingredients.name as ingredient', 'recipe_ingredients.quantity', 'units.name as unit')
+    .from('recipe_ingredients')
+    .leftJoin('recipes', 'recipes.id', 'recipe_ingredients.recipe_id')
+    .leftJoin('ingredients', 'ingredients.id', 'recipe_ingredients.ingredient_id')
+    .leftJoin('units', 'units.id', 'recipe_ingredients.unit_id')
+    .where({'recipes.id' : id})
 }
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -23,6 +23,49 @@ router.post("/", (req, res) => {
     })
 })
 
+//add ingredient to recipe one at a time
+router.post("/:id/ingredients", (req, res) => {
+    const newIngredient = {
+        recipe_id: req.params.id, 
+        ingredient_id: req.body.ingredient_id, 
+        quantity: req.body.quantity, 
+        unit_id: req.body.unit_id
+    }
+    Recipes.addIngredient(newIngredient) 
+    .then( id => {
+        res.status(201).json(id)
+    })
+    .catch( error => {
+        res.status(500).json(error)
+    })
+})
+
+router.get("/:id", (req, res) => {
+    Recipes.getRecipe(req.params.id)
+    .then( recipe => {
+        res.status(200).json(recipe)
+    })
+    .catch( error => {
+        res.status(500).json(error)
+    })
+})
+
+
+//middleware
+/*function validateRecipeId(req, res, next) {
+    Dishes.getDish(req.params.id) 
+    .then( dish => {
+        if(dish.length < 1) {
+            res.status(404).json({message: "dish not found"})
+        } else {
+            req.body.dish = dish
+            next();
+        }
+    })
+    .catch( error => {
+        res.status(500).json(error)
+    })
+}*/
 
 
 
